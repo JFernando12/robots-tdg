60c192df8ed020c8b8cf14c4c329b40af192a9c9
JFernando12 <fire.jf2012@gmail.com>
2022-10-24 10:37:58 -0500

    changes

--- src/robots/google-sheets/sendTrackingGuide.ts
+++ src/robots/google-sheets/sendTrackingGuide.ts
@@ -4,49 +4,66 @@ import { isGuideCreated, isGuideSentWhatsapp } from '../../settings/checks';
 import { ValuesGuideSentWhatsapp } from '../../interfaces/ValueChecks';
 import { whatsappClient } from '../../helpers/whatsapp/Whatsapp';
 import { PosGuide } from '../../interfaces/PositionsGuide';
-const getData = async () => {
+const getData = async (): Promise<Array<Array<string>>> => {
   let data = await currentSheet.get({
     nombre: 'prueba',
     celdaInicio: 'A2',
     celdaFinal: 'H1000',
   });
 
-  data = data
-    ?.filter((order) => order.length !== 0)
-    .filter((order) => isGuideCreated(order[PosGuide.guideCreated]));
+  if (!data) {
+    return [];
+  }
 
+  return data;
+};
+
+const notSentWhatsapp = (
+  data: Array<Array<string>>
+): Array<Array<string>> | [] => {
+  data = data
+    .filter((order) => isGuideCreated(order[PosGuide.guideCreated]))
+    .filter((order) => !isGuideSentWhatsapp(order[PosGuide.guideSentWhatsapp]));
   return data;
 };
 
 const sendTrackingGuide = async () => {
   let data = await getData();
+  if (!data) {
+    throw new Error('No hay data disponible');
+  }
+  let dataWhatsapp = notSentWhatsapp(data);
+  console.log(dataWhatsapp);
+
   await whatsappClient.start();
 
   let i = 0;
-  while (4 > i) {
-    let order = data![i];
+  while (dataWhatsapp.length > i) {
+    let order = dataWhatsapp[i];
     let noteOrder = order[PosGuide.noteOrder];
-    let guideSentEmail = order[PosGuide.guideSentEmail];
-    let guideSentWhatsapp = order[PosGuide.guideSentWhatsapp];
-    let guideCreated = order[PosGuide.guideCreated];
     const guide = order[PosGuide.guide];
-    const email = order[PosGuide.email];
     const whatsapp = order[PosGuide.whatsapp];
-    const orderId = order[PosGuide.orderId];
 
-    const acumNotes = '';
     try {
-      if (!isGuideSentWhatsapp(guideSentWhatsapp)) {
-        await whatsappClient.sendMessage(whatsapp, guide);
-        order[PosGuide.guideSentWhatsapp] = ValuesGuideSentWhatsapp.true;
-      }
+      await whatsappClient.sendMessage(whatsapp, guide);
+      order[PosGuide.guideSentWhatsapp] = ValuesGuideSentWhatsapp.true;
     } catch (error) {
       console.log(error);
     }
-    data![i] = order;
+    dataWhatsapp[i] = order;
     i++;
   }
 
+  // Actualizando data comparando con dataWhatsapp
+  dataWhatsapp.map((orderWhats) => {
+    const orderId = data?.findIndex(
+      (order) => order[PosGuide.orderId] === orderWhats[PosGuide.orderId]
+    );
+    data[orderId!][PosGuide.guideSentWhatsapp] =
+      orderWhats[PosGuide.guideSentWhatsapp];
+  });
+
+  // Preparando informacion que se va a mandar a Sheet
   const listSent = data?.map((order) => [
     order[PosGuide.guideSentWhatsapp],
     order[PosGuide.noteOrder],
@@ -56,7 +73,7 @@ const sendTrackingGuide = async () => {
     throw new Error('No hay nada para actualizar');
   }
 
-  // Update sheet
+  // Updating sheet
   await currentSheet.update(
     {
       nombre: 'prueba',
